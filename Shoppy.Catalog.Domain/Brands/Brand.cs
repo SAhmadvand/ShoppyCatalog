62d7909d9b4bf166c8255e167e5544bd3f537232
Saeed Ahmadvand <ahmadvand.saeed@yahoo.com>
2023-01-16 17:08:54 +0330

    Add Brand and Attribute aggregates

---
+++ Shoppy.Catalog.Domain/Brands/Brand.cs
@@ -0,0 +1,23 @@
+﻿using Shoppy.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shoppy.Catalog.Domain.Brands
+{
+    public class Brand : AggregateRoot<long>
+    {
+        #region Constructors
+        private Brand()
+        {
+        }
+
+        public Brand(long id)
+        {
+            Id = id;
+        }
+        #endregion
+    }
+}
--- Shoppy.Catalog.Domain/Categories/Factories.cs
+++ Shoppy.Catalog.Domain/Categories/Factories.cs
@@ -2,5 +2,8 @@ namespace Shoppy.Catalog.Domain.Domain.Categories;
 
 public partial class Category
 {
-    
+    public static Task<Category> CreateAsync()
+    {
+        return Task.FromResult((Category)default);
+    }
 }
